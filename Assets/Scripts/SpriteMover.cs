2294b5cb81e92a4a7222c961fa6c5d7beeeccf07
syd weaver <sydweave@uat.edu>
2020-01-23 17:35:36 -0700

    week 3 work

---
+++ Assets/Scripts/SpriteMover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMover : MonoBehaviour
+{
+    private Transform tf;
+
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move up if the player presses up
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) // "||" means or 
+        { 
+            //only works if shift is held down
+        }
+        else
+        {
+            //only works if shift is not held down
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.position = tf.position + Vector3.up * Time.deltaTime * speed;
+        }
+
+    }
+}
